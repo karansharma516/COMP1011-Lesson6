b968ae304d19f326ac487d35146bae4318402cb3
karan sharma <karan.sharma516@yahoo.com>
2015-02-12 03:43:10 -0800

    Created Vehicle Abstract Class

---
+++ src/Program.java
@@ -0,0 +1,12 @@
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Object myObject = new Object();
+		String mystring = myObject.toString();
+		System.out.println(mystring);
+
+	}
+
+}
---
+++ src/Vehicle.java
@@ -0,0 +1,31 @@
+
+public abstract class Vehicle {
+
+	// INSTANCE VARIABLES (FIELDS) +++++++++++++++++++++++++++++++++++++++++++++
+	private int _tireCount;
+
+	/**
+	 * @return the _tireCount
+	 */
+	public int getTireCount() {
+		return _tireCount;
+	}
+
+
+
+	/**
+	 * @param _tireCount the _tireCount to set
+	 */
+	public void setTireCount(int _tireCount) {
+		this._tireCount = _tireCount;
+	}
+	
+	
+	// CONSTRUCTOR +++++++++++++++++++++++++++++++++++++++++
+	public void Vehicle()
+	{
+		
+	}
+
+
+}
